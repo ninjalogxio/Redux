c206b2b7bf2c0d5f2f66f07e20dd06bfa1c382d5
codeswifter <xiaolong.jin@icloud.com>
2023-04-11 23:12:39 +0800

    removeDuplicates

--- Sources/SwiftUI-Redux/_Store.swift
+++ Sources/SwiftUI-Redux/_Store.swift
@@ -9,61 +9,49 @@ import Combine
 import Redux
 import SwiftUI
 
-internal class _Store<State> : ObservableObject, StoreProtocol {
+internal class _Store<StoreType> : ObservableObject, StoreProtocol where StoreType : StoreProtocol {
     
-    @Published internal var state: State
+    public typealias State = StoreType.State
     
-    private let _getState: () -> State
+    private let _store: StoreType
     
-    private let _dispatch: Dispatch
+    private let _removeDuplicates: Bool
     
-    private var disposables: Set<AnyDisposable> = []
-    
-    internal init<StoreType>(_ store: StoreType) where StoreType : StoreProtocol, StoreType.State == State {
-        _getState = store.getState
-        _dispatch = store.dispatch(_:)
-        self.state = _getState()
-        
-        store.addObserver(Downstream(parent: self)).store(in: &disposables)
+    internal init(_ store: StoreType, removeDuplicates: Bool = true) {
+        _store = store
+        _removeDuplicates = removeDuplicates
     }
     
     internal func getState() -> State {
-        return _getState()
-    }
-    
-    internal func dispatch(_ action: Action) {
-        _dispatch(action)
+        return _store.getState()
     }
     
     internal func addObserver<O>(_ observer: O) -> Disposable where O : Observer, State == O.Value {
-        let cancellable = $state.sink(receiveValue: observer.receive(_:))
-        return AnyDisposable(cancellable.cancel)
+        return _store.addObserver(observer)
     }
     
-    func addObserver<O, S>(_ observer: O, transform: @escaping (Transform<State>) -> Transform<S>) -> Disposable where O : Observer, S == O.Value {
-        let first = Transform<State>()
-        transform(first).update{ observer.receive($1) }
-        
-        let cancellable = $state
-            .scan(Optional<(State?, State)>.none) { ($0?.1, $1) }
-            .compactMap { $0 }
-            .sink(receiveValue: first.receive)
-        return AnyDisposable(cancellable.cancel)
+    internal func addObserver<O, S>(_ observer: O, transform: @escaping (Transform<State>) -> Transform<S>) -> Disposable where O : Observer, S == O.Value {
+        return _store.addObserver(observer, transform: transform)
     }
 }
 
 extension _Store {
     
-    private class Downstream : Observer {
+    internal func dispatch(_ action: Action) {
+        _store.dispatch(action)
+        objectWillChange.send()
+    }
+}
+
+extension _Store where State : Equatable {
+    
+    internal func dispatch(_ action: Action) {
+        let oldValue = _store.getState()
+        _store.dispatch(action)
+        let newValue = _store.getState()
         
-        unowned var parent: _Store
-        
-        init(parent: _Store) {
-            self.parent = parent
-        }
-        
-        func receive(_ value: State) {
-            parent.state = value
+        if !_removeDuplicates || oldValue != newValue {
+            objectWillChange.send()
         }
     }
 }
